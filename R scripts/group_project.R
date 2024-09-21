953172aa6a88adc960328d5fd904c73530e9ae23
erikaw7 <160258837+erikaw7@users.noreply.github.com>
2024-09-21 11:44:37 -0400

    Add files via upload
    Updated R script

--- R scripts/group_project.R
+++ R scripts/group_project.R
@@ -4,29 +4,51 @@
 # Libraries
 library(tidyverse)
 library(readxl)
-library(sf)
-library(terra)
-library(spData)
-library(tmap)
+library(ggplot2)
+
 
 # Load in data
-cbus_tracts <- sf::st_read("C:/Users/erika/OneDrive - The Ohio State University/PhD/Courses/Ent Techniques fall 2024/Group Project/Data/Columbus_TreeCanopy_2021_GDB_20230817/Columbus_TreeCanopy_2021.gdb", 
-                           layer = "Cols_Tracts_AOI")
+trees_mosquitoes<- read_excel("C:/Users/erika/OneDrive - The Ohio State University/PhD/Courses/Ent Techniques fall 2024/Group Project/Data/mosquito_trap_locations_with_trees.xlsx")
 
-cbus_land_cover<- read.csv("C:/Users/erika/OneDrive - The Ohio State University/PhD/Courses/Ent Techniques fall 2024/Group Project/Data/cbuslandcovertracts.csv",
-                           header = TRUE)
-
-summary(cbus_land_cover)
-
-mosquitoes<- read_excel("C:/Users/erika/OneDrive - The Ohio State University/PhD/Courses/Ent Techniques fall 2024/Group Project/Data/HRD Data.xlsx")
+mosquitoes<- read.csv("C:/Users/erika/OneDrive - The Ohio State University/PhD/Courses/Ent Techniques fall 2024/Group Project/Data/2021_mosquito_trap_data.csv")
 summary(mosquitoes)
 
-tracts_landcover_merge<- merge(cbus_land_cover, cbus_tracts, by = "TC_ID")
-
 # Variable corrections
-tracts_landcover_merge$TC_ID_fac<- as.factor(tracts_landcover_merge$TC_ID)
+mosquitoes$x <- mosquitoes$longitude
+mosquitoes$y<- mosquitoes$latitude
+
+mosquitoes$zone_name<- mosquitoes$zone.name
+trees_mosquitoes$zone_name<- trees_mosquitoes$`Zone Name`
+
+# Cleaning data so we can get count of trees at buffered traps and count of 
+  # mosquitoes at buffered traps
+
+mosquitoes_sub<- mosquitoes %>% group_by(zone_name, trap.type) %>%
+  summarise(sum_bugs = sum(total.count))
+
+mosquitoes_sub<- na.omit(mosquitoes_sub)
+
+trees_mosquitoes_sub<- subset(trees_mosquitoes, select = c("zone_name", "OBJECTID_count",
+                                                           "x", "y"))
+
+trees_bugs_merge<- merge(mosquitoes_sub, trees_mosquitoes_sub, by = "zone_name", 
+                         all.mosquitoes_sub = FALSE, all.trees_mosquitoes_sub = FALSE)
+
+trees_bugs_unique<- trees_bugs_merge %>%
+  distinct(trap.type, zone_name, .keep_all = TRUE)
+
+# More variable corrections
+trees_bugs_unique$tree_count<- trees_bugs_unique$OBJECTID_count
 
 # Visualize
-ggplot(tracts_landcover_merge, aes(x = TC_ID_fac, y = Can_P)) +
+ggplot(mosquitoes, aes(x = zone_name, y = total.count)) +
   geom_bar(stat = "identity") +
+  theme_classic()
+
+ggplot(trees_bugs_unique, aes(x = tree_count, y = sum_bugs)) +
+  geom_point() +
+  theme_classic()
+
+ggplot(trees_bugs_unique, aes(x = tree_count, y = sum_bugs, col = tree_count)) +
+  geom_point() +
   theme_classic()
